b53d1a70febc3ea7c13690794c56da63aa6720f1
Filipa <filipatitan@gmail.com>
2022-01-07 11:46:40 +0000

    minor corrections in client

--- src/tvg/client/Client.java
+++ src/tvg/client/Client.java
@@ -1,8 +1,8 @@
 package tvg.client;
 
 import tvg.board.Frame;
+import tvg.common.UpdateMessages;
 import tvg.game.Game;
-import tvg.player.Player;
 
 import java.io.*;
 import java.net.InetAddress;
@@ -10,12 +10,11 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    // OPEN A CLIENT SOCKET
+
     Scanner scan = new Scanner(System.in);
     InetAddress hostName;
     int portNumber;
     Socket serverSocket;
-    Thread receiveUpdate;
 
     Game game;
     ObjectOutputStream objectOutputStream;
@@ -26,21 +25,22 @@ public class Client {
 
 
     public void getServerInfo() throws IOException {
-        System.out.print("Server IP: ");
+        print("Server IP: ");
         String host = scan.nextLine();
         this.hostName = InetAddress.getByName(host);
-        System.out.print("Port: ");
+
+        print("Port: ");
         portNumber = scan.nextInt();
         scan.nextLine();
         serverSocket = new Socket(hostName, portNumber);
-        System.out.println("*connection established*");
+
+        print("*connection established*");
     }
 
-    public void speak() throws IOException, ClassNotFoundException, InterruptedException {
+    public void speak() throws IOException, ClassNotFoundException {
 
         BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
         PrintWriter out = new PrintWriter(serverSocket.getOutputStream(), true);
-
 
         while (serverSocket.isBound()) {
 
@@ -53,22 +53,20 @@ public class Client {
 
             if (received.contains("init")) {
 
-                System.out.println("receiving game ...");
                 objectInputStream = new ObjectInputStream(serverSocket.getInputStream());
                 Object object = objectInputStream.readObject();
 
                 if (object instanceof Game) {
                     game = (Game) object;
                 }
-                System.out.println("recebi um jogo " + game);
-                System.out.println("You joined a game!");
+                print(UpdateMessages.RECEIVED_GAME + game);
                 frame = new Frame(game);
                 frame.start();
 
                 playingLoop();
             }
 
-            System.out.println(received);
+            print(received);
 
             String message = scan.nextLine();
             out.println(message);
@@ -85,31 +83,22 @@ public class Client {
         while (true) {
             frame.repaint();
             game.getGameBoard().updateUI();
+
             if (!game.getCurrentPlayer().getName().equals(name)) {
                 game.turnOffOtherPlayerButtons();
             }
 
-            for(Player p : game.playerList){
-                System.out.println(p.getName());
-            }
-
-            System.out.println(game.getCurrentPlayer().getName());
-
             if (game.getCurrentPlayer().getName().equals(name)) {
-                System.out.println("passei o receive game");
 
                 while (!game.getCurrentPlayer().isEndOfTurn()) {
 
                 }
 
-                System.out.println("out of while");
                 if (game.getCurrentPlayer().isEndOfTurn()) {
-                    System.out.println("entrou no is end of turn");
                     game.resetEndOfTurn();
                     game.setCurrentPlayer(game.playerList.get(game.playerIndex));
                     game.getGameBoard().rounds.setText(game.getCurrentPlayer().getName());
                     sendGameAfterTurn();
-                    System.out.println(game.getCurrentPlayer());
                     continue;
                 }
             }
@@ -119,20 +108,18 @@ public class Client {
     }
 
     public void receiveGame() throws IOException, ClassNotFoundException {
-        System.out.println("entrei no receive game");
         objectInputStream = new ObjectInputStream(serverSocket.getInputStream());
 
         Object object = objectInputStream.readObject();
 
         if (object instanceof Game) {
-            System.out.println("olha que entrou ehehe");
             game = (Game) object;
             frame.setGame(game);
             game.getGameBoard().updateUI();
             frame.validate();
         }
-        System.out.println("recebi um jogo " + game);
 
+        print(UpdateMessages.RECEIVED_GAME + game);
     }
 
     public void sendGameAfterTurn() throws IOException {
@@ -140,14 +127,17 @@ public class Client {
         objectOutputStream.writeObject(game);
         objectOutputStream.flush();
 
-        System.out.println("enviei um jogo " + game);
+        print(UpdateMessages.SENT_GAME + game);
     }
 
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Client client = new Client();
         client.getServerInfo();
         client.speak();
+    }
+
+    public void print(String message) {
+        System.out.println(message);
     }
 }
 
