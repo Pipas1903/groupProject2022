47457fc8154a2fe057fd46e9cd2214fbb5cf06c7
Filipa <filipatitan@gmail.com>
2022-01-07 11:48:00 +0000

    added class update message

--- src/tvg/common/UpdateMessages.java
+++ src/tvg/common/UpdateMessages.java
@@ -1,2 +1,8 @@
-package tvg.common;public class UpdateMessages {
+package tvg.common;
+
+public class UpdateMessages {
+    public static final String WAITING_FOR_CLIENT = "WAITING FOR CLIENT";
+    public static final String CLIENT_CONNECTED = "CLIENT CONNECTED - ";
+    public static final String RECEIVED_GAME = "RECEIVED A GAME ";
+    public static final String SENT_GAME = "SENT A GAME ";
 }
