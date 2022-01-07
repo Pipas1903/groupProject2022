2e1ba30f6852a7d90908bdf7371acc1a55b6a67e
Filipa <filipatitan@gmail.com>
2022-01-07 12:24:37 +0000

    addition of messages

--- src/tvg/common/Messages.java
+++ src/tvg/common/Messages.java
@@ -1,8 +1,23 @@
-package tvg.game;
+package tvg.common;
 
 public class Messages {
     public static final String ROUND = "ROUND ";
     public static final String PLAYER_TURN = ", it's your turn!";
     public static final String THROW_DICE = ", throw your dice!";
     public static final String DICE_FACE = "You got ";
+    public static final String INSERT_NAME = "Insert your name: ";
+    public static final String STOP = "stop";
+    public static final String QUESTION_CREATE_OR_JOIN = "Do you wish to create or join a game?";
+    public static final String CREATE_GAME = "1 - create a game";
+    public static final String JOIN_GAME = "2 - join a game";
+    public static final String INSERT_GAME_NAME = "Insert a name for your game: ";
+    public static final String GAME_CREATED = "Game created successfully!";
+    public static final String CHOOSE_NUMBER_OF_PLAYERS = "How many players will there be? Choose a number between 2 and 4";
+    public static final String NUMBER_OF_PLAYERS_CHOSEN = "Great, there will be ";
+    public static final String PLAYERS = " players";
+    public static final String PRESS_ENTER = "---press enter to continue---";
+    public static final String INICIATE_GAME = "init";
+    public static final String REFRESH = "If there's no games, press 'R' to refresh";
+    public static final String WELCOME_TO_GAME= "Welcome to ";
+
 }
--- src/tvg/common/UpdateMessages.java
+++ src/tvg/common/UpdateMessages.java
@@ -5,4 +5,9 @@ public class UpdateMessages {
     public static final String CLIENT_CONNECTED = "CLIENT CONNECTED - ";
     public static final String RECEIVED_GAME = "RECEIVED A GAME ";
     public static final String SENT_GAME = "SENT A GAME ";
+    public static final String CLIENT = "CLIENT ";
+    public static final String WROTE_NAME = " WROTE THEIR NAME";
+    public static final String CREATED_GAME = " CREATED GAME CALLED ";
+    public static final String JOINED_GAME= " JOINED GAME ";
+
 }
